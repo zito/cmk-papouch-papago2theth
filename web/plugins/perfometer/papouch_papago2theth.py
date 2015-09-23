b6819373e79a0d811afeabdfb5c3f0aae2ef01d2
Václav Ovsík <vaclav.ovsik@gmail.com>
2015-09-23 17:00:24 +0200

    Initial commit

---
+++ web/plugins/perfometer/papouch_papago2theth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# vim:sta:si:sw=4:sts=4:et:
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2015             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# The Check_MK official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+# Check has been developed using:
+# Papago 2TH ETH - Thermo / Humidity / Dew point meter with eth if
+# Device Firmware Version   1.4/6
+# http://www.papouch.com/en/shop/product/papago-2th-eth-temperature-and-humidity-meter-with-ethernet/
+#
+# +------------------------------------------------------------------+
+# | This file has been contributed by:                               |
+# |                                                                  |
+# | Václav Ovsík <vaclav.ovsik@gmail.com>             Copyright 2015 |
+# +------------------------------------------------------------------+
+
+
+def perfometer_check_mk_papouch_papago2theth(row, check_command, perf_data):
+    info = row["service_plugin_output"]
+    color = '#00ff00'
+    if info.startswith('WARN'):
+        color = '#ffff00'
+    if info.startswith('CRIT'):
+        color = '#ff0000'
+    valunit = info.split()[-1]
+    val = float(valunit.rstrip(u'°C%'))
+    if 'Humidity' in info:
+        return valunit, perfometer_linear(val, color)
+    if 'Dew' in info:
+        return valunit, perfometer_logarithmic(val, 10, 1.2, color)
+    return valunit, perfometer_logarithmic(val, 21, 1.2, color)
+
+perfometers['check_mk-papouch_papago2theth'] = perfometer_check_mk_papouch_papago2theth
